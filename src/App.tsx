1f7ee449e2cb26c43fcff6bd8415aefad29c135a
Siddhant Tandon <cmxiv914@gmail.com>
2024-08-08 21:41:38 +0200

    New blog site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import './App.scss'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import BlogPost from "./BlogPost";
 
 function App() {
-    return <>Foo bar! Biz Baz!</>
+    const [blogs, setBlogs] = useState<string[]>([])
+
+    useEffect(() => {
+        axios
+            .get('/blog/manifest.json')
+            .then(response => {
+                setBlogs(response.data.blogs)
+            })
+            .catch(reason => {
+                console.error(reason)
+            });
+    }, []);
+
+    return <>
+        <main className="main-content">
+            <section className="blog-content">
+                {
+                    blogs
+                        .sort()
+                        .map( (blog, index) => <BlogPost key={`blog-post-${index}`} fileNameWithExt={blog} />)
+                }
+            </section>
+            <aside className="sidebar-container">
+                <img className="headshot" src="/headshot.jpeg" alt="Insert super amazing headshot"/>
+                <section className="sidebar">
+                    <ul>
+                        <li><a href="https://tandosid.dev">Blog</a></li>
+                        <li><a href="https://github.com/cmxiv">GitHub</a></li>
+                        <li><a href="https://linkedin.com/in/siddhanttandon">LinkedIn</a></li>
+                        <li><a href="mailto:me@tandosid.dev">Email</a></li>
+                    </ul>
+                </section>
+            </aside>
+        </main>
+    </>
 }
 
 export default App
